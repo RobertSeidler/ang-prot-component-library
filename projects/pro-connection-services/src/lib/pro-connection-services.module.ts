9ce46458dcc2537e0b0e615e14d0f2b9a9784277
Robert Seidler <r.seidler@protronic-gmbh.de>
2022-12-14 14:27:27 +0100

    merge

--- projects/pro-connection-services/src/lib/pro-connection-services.module.ts
+++ projects/pro-connection-services/src/lib/pro-connection-services.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 
+// import { Lsm6Service } from 'pro-connection-services';
+
 @NgModule({
   declarations: [],
   imports: [],
+  providers: [],
   exports: []
 })
 export class ProConnectionServicesModule { }
+
+// export * from 'projects/pro-connection-services/src/lib/ble.service';
+// export * from 'projects/pro-connection-services/src/lib/cloud.service';
+// export * from 'projects/pro-connection-services/src/lib/lsm6.service';
+// export * from 'projects/pro-connection-services/src/lib/nfc.service';
+
